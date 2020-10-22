8154bb58631f16a74cdde6a67112c8296959d170
Aram2142 <andresaaramirez@aol.com>
2020-10-22 14:08:34 -0400

    index.js and css file missing

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,51 +1,54 @@
-//console.log("Hi from your service-worker.js file!");
-
-const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/styles.css",
-    "/index.js",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png",
-    "/manifest.webmanifest",
-];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/assets/css/styles.css",
+    "/assets/js/index.js",
+    "/assets/icons/icon-192x192.png",
+    "/assets/icons/icon-512x512.png",
+    "/manifest.webmanifest",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
+];
+
 // install
-self.addEventListener("install", function (event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log("Your files were pre-cached successfully!");
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
-
-    self.skipWaiting();
-});
-
-self.addEventListener("activate", function (evt) {
+self.addEventListener("install", function(evt) {
     evt.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(
-                keyList.map(key => {
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log("Removing old cache data", key);
-                        return caches.delete(key);
-                    }
-                })
-            );
-        })
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
     );
+  
+    self.skipWaiting();
+  });  
 
+  //activate
+  self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
     self.clients.claim();
-});
+  });
+  
 
 
 self.addEventListener('fetch', function (event) {
     console.log(event.request.url);
-    if (event.request.url.includes("api/")) {
+    //if (event.request.url.includes("/api/")) {
         event.respondWith(
             caches.open(DATA_CACHE_NAME).then(cache => {
                 return fetch(event.request)
@@ -61,13 +64,13 @@ self.addEventListener('fetch', function (event) {
             }).catch(err => console.log(err))
         );
         return;
-    }
-
+    //}
+/*
     event.respondWith(
         caches.match(event.request).then(function (response) {
             return response || fetch(event.request);
         })
-    );
+    );*/
 
 
 });
