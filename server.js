8b86b0e5c69f18fa6b0b17525c2254958e191fe9
Aram2142 <andresaaramirez@aol.com>
2020-10-25 12:55:29 -0400

    see if it works

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB || "mongodb://localhost/budget",
+  process.env.MONGODB_URI || "mongodb://localhost/budget",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
